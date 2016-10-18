4b6159a165d98c9aed07e78decdc711f57deb9f9
Vitor Rangel <rangel.final@gmail.com>
2016-10-18 09:07:39 -0200

    Add eslint and babel to gulp

---
+++ tasks/js.babel.js
@@ -0,0 +1,13 @@
+import gulp from 'gulp';
+import eslint from 'gulp-eslint';
+import babel from 'gulp-babel';
+import paths from './paths';
+
+gulp.task('js', () =>
+  gulp.src(paths.src.js)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(gulp.dist(paths.dist.js))
+);
